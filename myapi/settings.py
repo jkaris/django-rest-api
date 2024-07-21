c7912d7897af873d3b4bc02046830b24a5c8e589
Maragwa Mercy <mercymaragwa@gmail.com>
2024-07-21 20:13:07 +0300

    added djangorest framework

--- myapi/settings.py
+++ myapi/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "users",
 ]
 
@@ -124,3 +125,14 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.CustomUser"
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'users.authentication.UserAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
+
